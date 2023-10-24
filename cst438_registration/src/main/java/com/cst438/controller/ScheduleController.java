a95e6e0695f773e8ae3b53850f5045e6ccaac063
felixflores1 <123616125+felixflores1@users.noreply.github.com>
2023-10-24 15:00:02 -0700

    Update ScheduleController.java JWT Security

--- cst438_registration/src/main/java/com/cst438/controller/ScheduleController.java
+++ cst438_registration/src/main/java/com/cst438/controller/ScheduleController.java
@@ -41,65 +41,57 @@ public class ScheduleController {
 	 * get current schedule for student.
 	 */
 	@GetMapping("/schedule")
-	public ScheduleDTO[] getSchedule( @RequestParam("year") int year, @RequestParam("semester") String semester ) {
-		System.out.println("/schedule called.");
-		String student_email = "test@csumb.edu";   // student's email 
-		
-		Student student = studentRepository.findByEmail(student_email);
-		if (student != null) {
-			System.out.println("/schedule student "+student.getName()+" "+student.getStudent_id());
-			List<Enrollment> enrollments = enrollmentRepository.findStudentSchedule(student_email, year, semester);
-			ScheduleDTO[] sched = createSchedule(year, semester, student, enrollments);
-			return sched;
-		} else {
-			return new ScheduleDTO[0];   // return empty schedule for unknown student.
-		}
-	}
+   	 public ScheduleDTO[] getSchedule(@RequestParam("year") int year, @RequestParam("semester") String semester, Principal principal) {
+        	String student_email = principal.getName(); // Get the user's email from Principal
+
+        	Student student = studentRepository.findByEmail(student_email);
+        	if (student != null) {
+            		List<Enrollment> enrollments = enrollmentRepository.findStudentSchedule(student_email, year, semester);
+            		ScheduleDTO[] sched = createSchedule(year, semester, student, enrollments);
+            		return sched;
+        	} else {
+            	return new ScheduleDTO[0];
+        	}
+    	}
 	/*
 	 * add a course for a student
 	 */
 	@PostMapping("/schedule/course/{id}")
 	@Transactional
-	public ScheduleDTO addCourse( @PathVariable int id  ) { 
-		String student_email = "test@csumb.edu";   // student's email 
-		Student student = studentRepository.findByEmail(student_email);
-		Course course  = courseRepository.findById(id).orElse(null);
-		// student.status
-		// = 0  ok to register.  != 0 registration is on hold.		
-		if (student!= null && course!=null && student.getStatusCode()==0) {
-			// TODO check that today's date is not past add deadline for the course.
-			Enrollment enrollment = new Enrollment();
-			enrollment.setStudent(student);
-			enrollment.setCourse(course);
-			enrollment.setYear(course.getYear());
-			enrollment.setSemester(course.getSemester());
-			enrollmentRepository.save(enrollment);
-			// notify grade book of new enrollment event
-			gradebookService.enrollStudent(student_email, student.getName(), course.getCourse_id());
-			ScheduleDTO result = createSchedule(enrollment);
-			return result;
-		} else {
-			throw  new ResponseStatusException( HttpStatus.BAD_REQUEST, "Course_id invalid or student not allowed to register for the course.  "+id);
-		}	
-	}
+	public ScheduleDTO addCourse(@PathVariable int id, Principal principal) {
+        	String student_email = principal.getName();
+        	Student student = studentRepository.findByEmail(student_email);
+        	Course course = courseRepository.findById(id).orElse(null);
+
+        	if (student != null && course != null && student.getStatusCode() == 0) {
+            		Enrollment enrollment = new Enrollment();
+            		enrollment.setStudent(student);
+            		enrollment.setCourse(course);
+            		enrollment.setYear(course.getYear());
+            		enrollment.setSemester(course.getSemester());
+            		enrollmentRepository.save(enrollment);
+            		gradebookService.enrollStudent(student_email, student.getName(), course.getCourse_id());
+            		ScheduleDTO result = createSchedule(enrollment);
+            		return result;
+        	} else {
+            	throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Course_id invalid or student not allowed to register for the course. " + id);
+        	}
+    	}	
 	/*
 	 * drop a course from student schedule
 	 */
 	@DeleteMapping("/schedule/{enrollment_id}")
 	@Transactional
-	public void dropCourse(  @PathVariable int enrollment_id  ) {
-		String student_email = "test@csumb.edu";   // student's email 
-		// TODO  check that today's date is not past deadline to drop course.
-		Enrollment enrollment = enrollmentRepository.findById(enrollment_id).orElse(null);
-		// verify that student is enrolled in the course.
-		if (enrollment!=null && enrollment.getStudent().getEmail().equals(student_email)) {
-			// OK.  drop the course.
-			 enrollmentRepository.delete(enrollment);
-		} else {
-			// something is not right with the enrollment.  
-			throw  new ResponseStatusException( HttpStatus.BAD_REQUEST, "Enrollment_id invalid. "+enrollment_id);
-		}
-	}
+    	public void dropCourse(@PathVariable int enrollment_id, Principal principal) {
+        	String student_email = principal.getName();
+        	Enrollment enrollment = enrollmentRepository.findById(enrollment_id).orElse(null);
+
+        	if (enrollment != null && enrollment.getStudent().getEmail().equals(student_email)) {
+            enrollmentRepository.delete(enrollment);
+        	} else {
+            	throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Enrollment_id invalid. " + enrollment_id);
+        	}
+   	 }
 	
 	/* 
 	 * helper method to transform course, enrollment, student entities into 
